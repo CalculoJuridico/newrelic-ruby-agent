0d19336a259bb9a3c2392b015b47a75f70deebe7
Caio Filipe de Moraes Rodrigues <caiofilipemr@gmail.com>
2023-08-09 17:36:48 -0300

    add: json parsing to better log

--- lib/new_relic/agent/utilization/vendor.rb
+++ lib/new_relic/agent/utilization/vendor.rb
@@ -63,7 +63,7 @@ module NewRelic
               false
             end
           rescue => e
-            NewRelic::Agent.logger.info("Metadata response: #{response}", e)
+            NewRelic::Agent.logger.info("Metadata response: #{JSON.parse(response.body)}", e)
             NewRelic::Agent.logger.error("Error occurred detecting: #{vendor_name}", e)
             record_supportability_metric
             false
